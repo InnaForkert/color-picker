32e88d38831658b1fecd0fc2be189a192dfdacad
Inna Forkert <inna.forkert@gmail.com>
2023-04-15 12:52:42 +0300

    feat: copy css btn

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { reactive, computed } from "vue";
+import { reactive, computed, ref } from "vue";
 
 //top-right-corner color
+const btnText = ref('Copy CSS');
+
 const color = reactive({
   fromRange: 0,
   hex: "#ffffff",
@@ -97,6 +99,17 @@ function computeColorFromRange() {
   computeFinalColor(coords.x, coords.y);
 }
 
+function copyGradient() {
+  navigator.clipboard.writeText(bcgColor.value) // Write the text to clipboard
+    .then(() => {
+      btnText.value = 'Copied!';
+      setTimeout(() => btnText.value = 'Copy CSS', 2500);
+    })
+    .catch((err) => {
+      console.error('Unable to copy to clipboard:', err);
+    });
+}
+
 function computeHex() {
   finalColor.hex = `#${finalColor.r.toString(16)}${finalColor.g.toString(
     16
@@ -187,6 +200,7 @@ export default {
         <label>Deg°<input type="number" v-model="gradient.angle" @input="computeGradient" min="0" max="360"
             step="1" /></label>
       </div>
+      <button class="btn" @click="copyGradient">{{ btnText }}</button>
     </div>
   </div>
 </template>
@@ -202,6 +216,76 @@ body {
   box-sizing: border-box;
 }
 
+.btn {
+  position: relative;
+  width: 100px;
+  top: 50px;
+  right: 50px;
+  align-items: center;
+  appearance: none;
+  background-color: #fff;
+  border-radius: 24px;
+  border-style: none;
+  box-shadow: rgba(0, 0, 0, .2) 0 3px 5px -1px, rgba(0, 0, 0, .14) 0 6px 10px 0, rgba(0, 0, 0, .12) 0 1px 18px 0;
+  box-sizing: border-box;
+  color: #3c4043;
+  cursor: pointer;
+  display: inline-flex;
+  fill: currentcolor;
+  font-family: "Google Sans", Roboto, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  height: 48px;
+  justify-content: center;
+  letter-spacing: .25px;
+  line-height: normal;
+  overflow: visible;
+  position: relative;
+  text-align: center;
+  text-transform: none;
+  transition: box-shadow 280ms cubic-bezier(.4, 0, .2, 1), opacity 15ms linear 30ms, transform 270ms cubic-bezier(0, 0, .2, 1) 0ms;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  will-change: transform, opacity;
+  z-index: 0;
+}
+
+.btn:hover {
+  background: #F6F9FE;
+  color: #174ea6;
+}
+
+.btn:active {
+  box-shadow: 0 4px 4px 0 rgb(60 64 67 / 30%), 0 8px 12px 6px rgb(60 64 67 / 15%);
+  outline: none;
+}
+
+.btn:focus {
+  outline: none;
+  border: 2px solid #4285f4;
+}
+
+.btn:not(:disabled) {
+  box-shadow: rgba(60, 64, 67, .3) 0 1px 3px 0, rgba(60, 64, 67, .15) 0 4px 8px 3px;
+}
+
+.btn:not(:disabled):hover {
+  box-shadow: rgba(60, 64, 67, .3) 0 2px 3px 0, rgba(60, 64, 67, .15) 0 6px 10px 4px;
+}
+
+.btn:not(:disabled):focus {
+  box-shadow: rgba(60, 64, 67, .3) 0 1px 3px 0, rgba(60, 64, 67, .15) 0 4px 8px 3px;
+}
+
+.btn:not(:disabled):active {
+  box-shadow: rgba(60, 64, 67, .3) 0 4px 4px 0, rgba(60, 64, 67, .15) 0 8px 12px 6px;
+}
+
+.btn:disabled {
+  box-shadow: rgba(60, 64, 67, .3) 0 1px 3px 0, rgba(60, 64, 67, .15) 0 4px 8px 3px;
+}
+
 .color-picker-container {
   width: 100%;
   height: 100vh;
@@ -211,7 +295,6 @@ body {
   flex-wrap: wrap;
   gap: 20px;
   background: v-bind(bcgColor);
-
 }
 
 .color-picker-section {
